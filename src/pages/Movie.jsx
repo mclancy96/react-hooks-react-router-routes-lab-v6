4e6cc17bd1762510d5bbfcec3de47794c715b851
Mike <mclancy96@gmail.com>
2025-06-17 10:24:43 -0400

    Movie page getting and displaying user correctly

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,7 +1,20 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import NavBar from "../components/NavBar";
 
 function Movie() {
+	const params = useParams();
+	const [movie, setMovie] = useState({});
+
+	useEffect(() => {
+		fetch(`http://localhost:4000/movies/${params.id}`)
+			.then((r) => r.json())
+			.then((fetchedMovie) => setMovie(fetchedMovie));
+	}, []);
+
+	if (!movie.id) {
+		return <h1>Loading Movie...</h1>;
+	}
 	return (
 		<>
 			<header>
@@ -9,6 +22,12 @@ function Movie() {
 			</header>
 			<main>
 				<h1>Movie</h1>
+				<p>{movie.id}</p>
+				<p>{movie.title}</p>
+				<p>{movie.time}</p>
+				{movie.genres.map((genre) => (
+					<p>{genre}</p>
+				))}
 			</main>
 		</>
 	);
